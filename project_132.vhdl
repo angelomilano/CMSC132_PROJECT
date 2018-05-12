b88bc2dc251d037cee7047bace87ddba0832d974
Maria Rowe R. Riomalos <mrriomalos@up.edu.ph>
2018-05-12 20:20:26 +0800

    values inserted in array & fetch flow established

--- project_132.vhdl
+++ project_132.vhdl
@@ -4,7 +4,8 @@ use IEEE.numeric_std.all;
 use STD.textio.all;
 
 entity project_132 is
-
+      --port (q: out std_logic_vector(1 downto 0); -- data output
+      --clk: in std_logic); -- clock signal
 end project_132;
 
 architecture behaviour of project_132 is
@@ -12,12 +13,12 @@ subtype ins_32 is std_logic_vector(31 downto 0);
 type ins_array is array(0 to 15) of ins_32;
 signal instructions : ins_array;
 
+
 begin
       process
             constant NUM_COL : integer := 31;
             file test_vector : text open read_mode is "input.txt";
             variable row : line;
-            variable temp : std_logic_vector(31 downto 0);
             variable row_counter : integer := 0;
             variable c: character;
 
@@ -27,21 +28,14 @@ begin
                         for i in 0 to NUM_COL loop
                               read(row, c);
                               case c is
-                                    when '0' => temp(i) := '0';
-                                    when '1' => temp(i) := '1';
-                                    when others => temp(i) := 'X';
+                                    when '0' => instructions(row_counter)(i) <= '0'; report "0";
+                                    when '1' => instructions(row_counter)(i) <= '1'; report "1";
+                                    when others => instructions(row_counter)(i) <= 'X'; report "X";
                               end case;
                         end loop;
-                        instructions(row_counter) := temp;
                         row_counter := row_counter + 1;
                   end loop;
                   file_close(test_vector);
                   wait;
-                  -- for kk in 1 to NUM_COL loop
-                  --      read(row,temp(kk));
-                  --end loop;
-
-                  --instructions(row_counter) <= std_logic_vector(temp,32);
-
       end process;
 end behaviour;
